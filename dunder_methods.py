a5eee682fa9364edb450f606a3fe33005c8f4e5e
brandonreedharris <brandonreedharris@gmail.com>
2021-05-17 15:29:04 -0400

    initial commit

---
+++ dunder_methods.py
@@ -0,0 +1,10 @@
+class Invoice:
+    def __init__(self, client, total):
+        self.client = client
+        self.total = total
+
+    def __str__(self):
+        return f'Invoice from {self.client} for {self.total}'
+
+inv = Invoice('Google', 500)
+print(str(inv))
